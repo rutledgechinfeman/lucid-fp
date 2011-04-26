7112f4ec280ce42727db5cadfcfc3e79da102ac8
rutledge.chin.feman@gmail.com <rutledge.chin.feman@gmail.com@cc3ac821-fe09-b41a-eedc-dc4eaaf1ceba>
2011-04-26 21:11:27 +0000

    whyyyy

--- trunk/src/factory.cpp
+++ trunk/src/factory.cpp
@@ -4,58 +4,46 @@
 
 using namespace std;
 
-Factory::Factory() {
-    m_texMap = map<string, QImage*>();
-    m_meshMap = map<string, Mesh*>();
+Factory::Factory()
+{
 }
 
-Factory::~Factory() {
-//TODO: delete stuff
+Factory::~Factory()
+{
+    for(map<string, QImage*>::iterator ii = m_texMap.begin(); ii != m_texMap.end(); ++ ii)
+    {
+        delete ii->second;
+    }
+    for(map<string, Mesh*>::iterator ii = m_meshMap.begin(); ii != m_meshMap.end(); ++ ii)
+    {
+        delete ii->second;
+    }
 }
 
 bool Factory::addFeatureType(string id, bool isTerminal, string geom, string dataPath)
 {
     // If it's already in the map, return false
-    if(m_featureListing.find(id) != m_featureListing.end())
-    {
-        return false;
-    }
+    if(m_featureListing.find(id) != m_featureListing.end()) { return false; }
 
     // Add a new listing
     m_featureListing[id] = FeatureProperties(id, isTerminal, geom, dataPath);
 
+    // Look for a datapath on terminal symbols only
+    if(!isTerminal   ) { return true; }
+    if(geom     == "") { cout << "Error: terminal symbol used without a specified geometry" << endl; }
+    if(dataPath == "") { cout << "Error: terminal symbol used without a specified datapath" << endl; }
 
-    //if it is not a terminal character then it doesn't have a texture or object attached
-    if(isTerminal){
-
-        //if it is a plane then it has a texture
-        if(geom == "plane"){
-            if(dataPath != ""){
-
-                if(m_texMap.find(dataPath) == m_texMap.end()){
-                    m_texMap[dataPath] = new QImage(QString(dataPath.c_str()));
-                }
-
-            }
-        }
-
-        //if it is a mesh then it has a .obj file
-        else if(geom == "mesh"){
-            if(dataPath != ""){
-
-                if(m_meshMap.find(dataPath) == m_meshMap.end()){
-                    m_meshMap[dataPath] = new Mesh(dataPath);
-                }
-
-            }
-
-        }
-
-        //add other stuff later
-        else{}
+    // Planes have textures (2D)
+    if(geom == "plane" && m_texMap.find(dataPath) == m_texMap.end())
+    {
+        m_texMap[dataPath] = new QImage(QString(dataPath.c_str()));
     }
-
-
+    // Meshes have... meshes. (3D)
+    else if(geom == "mesh" && m_meshMap.find(dataPath) == m_meshMap.end())
+    {
+        m_meshMap[dataPath] = new Mesh(dataPath);
+    }
+    else { cout << "Unknown geometry type: " << geom << endl; }
 
     return true;
 }
