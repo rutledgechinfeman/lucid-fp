1f6aa42ee439a7f6b6c68da2f7056dc086d0f177
rutledge.chin.feman@gmail.com <rutledge.chin.feman@gmail.com@cc3ac821-fe09-b41a-eedc-dc4eaaf1ceba>
2011-04-27 21:55:34 +0000

    Improved commenting and error handling/output across the board

--- trunk/src/mesh.cpp
+++ trunk/src/mesh.cpp
@@ -1,6 +1,4 @@
 #include "mesh.h"
-#include <boost/algorithm/string.hpp>
-
 #include <string>
 #include <fstream>
 #include <iostream>
@@ -11,79 +9,87 @@ using std::vector;
 using namespace std;
 
 
-Mesh::Mesh(string filename){
+Mesh::Mesh(string filename)
+{
     ifstream myfile(filename.c_str());
-    if(!myfile.is_open() || !myfile.good()){
-        //cout << "obj file fail" << endl;
+    if(!myfile.is_open() || !myfile.good())
+    {
+        cerr << "ERROR: Could not open mesh file: " << filename << endl;
     }
+
     vector<MeshVertex*> vertices;
     vector<int> triangles;
 
+    // Iterate over each line in the file
     string line;
     vector<string> tokens;
-    //for each line in the file
     int currVertex = 0;
     int currFace = 0;
-    while(myfile.good()){
-        tokens.clear();
+    while (myfile.good())
+    {
         getline(myfile, line);
         if (line.size() == 0) continue;
 
+        // Tokenize line
+        tokens.clear();
         StringUtil::split(line, " ", tokens);
-        //comment
-        if(tokens.at(0) == "#"){
-            continue;
-        }
 
-        else if(tokens.at(0) == "v"){
+        // Skip over commented lines
+        if(tokens.at(0) == "#") { continue; }
 
+        // Parse a vertex line
+        if(tokens.at(0) == "v")
+        {
+            if(tokens.size() != 4) { cerr << "WARNING: Skipping malformed vertex line in mesh file: " << line << endl; }
+
+            // Make a new vertex for this line
             MeshVertex* vert = new MeshVertex();
+            vert->idx = currVertex; // set ID
+            vert->p = double3(atoi(tokens[1].c_str()), atoi(tokens[2].c_str()), atoi(tokens[3].c_str())); // set coordinates
+            vertices.push_back(vert); // add to the list
 
-            vert->idx = currVertex;
-
-            if(tokens.size() < 4){
-                cout << "not enough coordinates in vertex number " << currVertex << endl;
-            }
-            vert->p = double3(atoi(tokens[1].c_str()), atoi(tokens[2].c_str()), atoi(tokens[3].c_str()));
-            vertices.push_back(vert);
             currVertex++;
         }
-        //vertex normal, vn
-        else if(tokens.at(0) == "n"){
-            //TODO: NORMAL STUFF?
-        }
-        //vertex texture, vt
-        else if(tokens.at(0) == "t"){
-            //TODO: TEXTURE STUFF?
-        }
 
-        //face stuff
-        else if(tokens.at(0) == "f"){
-            //FACE STUFF
-            if(tokens.size() != 4){
-                cout << "This may be a quad mesh" << endl;
-            }
+        // Parse a face line
+        else if(tokens.at(0) == "f")
+        {
+            if(tokens.size() != 4) { cerr << "WARNING: Skipping malformed vertex line in mesh file: " << line << endl; }
+
+            // Parse the vertices, of the form "vertex/normal/texture"
             vector<string> face;
-            for(unsigned int i=1; i<tokens.size(); i++){
+            for(unsigned int i=1; i<tokens.size(); i++)
+            {
                 face.clear();
                 StringUtil::split(tokens.at(i), "/", face);
-                triangles.push_back(atoi(face.at(0).c_str()));
+                triangles.push_back(atoi(face.at(0).c_str())); // Currently, we're only supporting the vertex numbers, not normals or textures
+
+                // TODO: if we ever implement this feature
+                if(tokens.size() > 1) { cerr << "WARNING: Currently, normals/textures are not supported for triangles. Skipping non-vertex data of: " << line << endl; }
             }
             currFace++;
         }
+        // Parse a vertex normal line
+        else if(tokens.at(0) == "n") { cerr << "WARNING: Currently, normals are not supported for mesh files. Skipping: " << line << endl; } // TODO
+        // Parse a vertex texture line
+        else if(tokens.at(0) == "t") { cerr << "WARNING: Currently, normals are not supported for mesh files. Skipping: " << line << endl; } // TODO
     }
 
+    // Wrap up
     myfile.close();
-    m_vertices = new MeshVertex[vertices.size()];
 
-    for(unsigned int i=0; i<vertices.size(); i++){
+    // Populate the master vertex list
+    m_vertices = new MeshVertex[vertices.size()];
+    for(unsigned int i=0; i<vertices.size(); i++)
+    {
         m_vertices[i].idx = vertices.at(i)->idx;
         m_vertices[i].p = vertices.at(i)->p;
     }
 
+    // Populate the master triangle list
     m_triangles = new MeshTriangle[triangles.size()/3];
-
-    for(unsigned int i=0; i<triangles.size(); i++) {
+    for(unsigned int i=0; i<triangles.size(); i++)
+    {
         m_triangles[i].v0 = &m_vertices[triangles[i*3]];
         m_triangles[i].v1 = &m_vertices[triangles[i*3+1]];
         m_triangles[i].v2 = &m_vertices[triangles[i*3+2]];
@@ -92,20 +98,24 @@ Mesh::Mesh(string filename){
     m_numtriangles = currFace;
     m_numvertices = vertices.size();
 
-    for(int i=0; i<m_numvertices; i++){
+
+    // TODO: remove, prints for testing purposes only
+    for(int i=0; i<m_numvertices; i++)
+    {
         cout << i << endl;
         cout << m_vertices[i].idx << ", " << m_vertices[i].p << endl;
     }
-
 }
 
-
-Mesh::Mesh(MeshVertex *vertexlist, int numvertices, int *trilist, int numtriangles) {
+// TODO: Would this constructor ever be used?
+Mesh::Mesh(MeshVertex *vertexlist, int numvertices, int *trilist, int numtriangles)
+{
     m_vertices = new MeshVertex[numvertices];
     memcpy(m_vertices, vertexlist, sizeof(MeshVertex) * numvertices);
     m_triangles = new MeshTriangle[numtriangles];
 
-    for(int i=0; i<numtriangles; i++) {
+    for(int i=0; i<numtriangles; i++)
+    {
         m_triangles[i].v0 = &m_vertices[trilist[i*3]];
         m_triangles[i].v1 = &m_vertices[trilist[i*3+1]];
         m_triangles[i].v2 = &m_vertices[trilist[i*3+2]];
@@ -116,29 +126,35 @@ Mesh::Mesh(MeshVertex *vertexlist, int numvertices, int *trilist, int numtriangl
 }
 
 
-Mesh::~Mesh() {
-
+Mesh::~Mesh()
+{
     delete[] m_triangles;
     delete[] m_vertices;
 }
 
-void Mesh::drawGL() {
+void Mesh::drawGL()
+{
     glBegin(GL_TRIANGLES);
-    for(int i=0; i < m_numtriangles; i++) {
+    for(int i=0; i < m_numtriangles; i++)
+    {
         glTexCoord2f(m_triangles[i].v0->t.x, m_triangles[i].v0->t.y);
         glVertex3f(m_triangles[i].v0->p.x, m_triangles[i].v0->p.y, m_triangles[i].v0->p.z);
+
         glTexCoord2f(m_triangles[i].v1->t.x, m_triangles[i].v1->t.y);
         glVertex3f(m_triangles[i].v1->p.x, m_triangles[i].v1->p.y, m_triangles[i].v1->p.z);
+
         glTexCoord2f(m_triangles[i].v2->t.x, m_triangles[i].v2->t.y);
         glVertex3f(m_triangles[i].v2->p.x, m_triangles[i].v2->p.y, m_triangles[i].v2->p.z);
     }
     glEnd();
 }
-/*
-void Mesh::drawTriangleIDs() {
+
+/*void Mesh::drawTriangleIDs()
+{
     glDisable(GL_TEXTURE_2D);
 
-    for(int i=0; i < m_numtriangles; i++) {
+    for(int i=0; i < m_numtriangles; i++)
+    {
         //convert i to rgb
         int k1 = i / (255 * 255);
         int k2 = (i - k1 * (255 * 255)) / (255);
@@ -152,5 +168,4 @@ void Mesh::drawTriangleIDs() {
     }
 
     glEnable(GL_TEXTURE_2D);
-}
-*/
+}*/
