d215f46a05329f59ae6d4c9146ebe18151a2f48b
hadrad1000@gmail.com <hadrad1000@gmail.com@cc3ac821-fe09-b41a-eedc-dc4eaaf1ceba>
2011-04-28 02:37:41 +0000

    More stuff with feature

--- trunk/src/feature.cpp
+++ trunk/src/feature.cpp
@@ -4,42 +4,55 @@ Feature::Feature()
 {
 }
 
-Feature::Feature(string symbol, bool isTerminal, bool isActive)
+Feature::Feature(string symbol, string geom, bool isActive, Feature* parent)
 {
-    //I think this is for identification. We may add id later
+    /// I think this is for identification. We may add id later
     m_symbol = symbol;
 
     /// Whether this feature has children or is in active use
     m_active = isActive;
 
-    /// Whether this symbol is a terminal or a nonterminal
-    m_terminal = isTerminal;
+
+    /// If this is the root then parent is NULL
+    m_parent = parent;
 
 
     //Set mesh and texture to null. SET THESE LATER IF YOU WANT TO USE THE MESH OR TEXTURE
     m_mesh = NULL;
     m_texture = NULL;
 
+
+    setType(geom);
+
+
+}
+
+
+void Feature::setType(string geom){
+    if(geom == "plane") m_type = PLANE;
+    if(geom == "mesh") m_type = MESH;
 }
 
 
 //This is here so that we don't have to include mesh and image in the constructor.
 //Convenient because feature may not have a mesh or a texture
-bool Feature::setMesh(Mesh* mesh){
+void Feature::setMesh(Mesh* mesh){
     m_mesh = mesh;
 }
 
-bool Feature::setTexture(QImage* image){
+void Feature::setTexture(QImage* image){
     m_texture = image;
+}
+
+void Feature::setActive(bool b){
+    m_active = b;
 }
 
 
 //Adds a feature child to the list of children
 //This is no longer a terminal if it ever was in the past
-bool Feature::addChild(Feature f){
+void Feature::addChild(Feature* f){
     m_children.push_back(f);
-
-    m_terminal = false;
 
 }
 
