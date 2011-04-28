d7df0d3486e3aaf436dc1b536504d7f09c58e677
hadrad1000@gmail.com <hadrad1000@gmail.com@cc3ac821-fe09-b41a-eedc-dc4eaaf1ceba>
2011-04-28 16:25:50 +0000

    Scope methods added

--- src/scope.cpp
+++ src/scope.cpp
@@ -1,9 +1,24 @@
 #include "scope.h"
 
 
-Scope::Scope(Vector4 scale, Matrix4x4 basis, Vector4 point){
+Scope::Scope(Vector4 point, Vector4 scale, Matrix4x4 basis){
     m_scale = scale;
     m_basis = basis;
+    m_point = point;
+    m_xbasis = Vector4(basis.a, basis.e, basis.i, basis.m);
+    m_ybasis = Vector4(basis.b, basis.f, basis.j, basis.n);
+    m_zbasis = Vector4(basis.c, basis.g, basis.k, basis.o);
+}
+
+Scope::Scope(Vector4 scale, Vector4 point, Vector4 xbasis, Vector4 ybasis, Vector4 zbasis){
+    m_scale = scale;
+    m_basis = Matrix4x4(xbasis.w, ybasis.w, zbasis.w,
+                        xbasis.x, ybasis.x, zbasis.x,
+                        xbasis.y, ybasis.y, zbasis.y,
+                        xbasis.z, ybasis.z, zbasis.z);
+    m_xbasis = xbasis;
+    m_ybasis = ybasis;
+    m_zbasis = zbasis;
     m_point = point;
 }
 
@@ -17,4 +32,25 @@ Vector4 Scope::getScale(){
 
 Matrix4x4 Scope::getBasis(){
     return m_basis;
+}
+
+
+Scope Scope::translate(Vector4 v){
+    return Scope(m_point+v, m_scale, m_basis);
+}
+
+Scope Scope::setSize(Vector4 v){
+    return Scope(m_point, v, m_basis);
+}
+
+Scope Scope::rotateX(REAL angle){
+    return Scope(m_point, m_scale, getRotMat(m_point, m_xbasis, angle) * m_basis);
+}
+
+Scope Scope::rotateY(REAL angle){
+    return Scope(m_point, m_scale, getRotMat(m_point, m_ybasis, angle) * m_basis);
+}
+
+Scope Scope::rotateZ(REAL angle){
+    return Scope(m_point, m_scale, getRotMat(m_point, m_zbasis, angle) * m_basis);
 }
--- src/scope.h
+++ src/scope.h
@@ -5,16 +5,26 @@
 class Scope
 {
 public:
-    Scope(Vector4 scale = Vector4(1,1,1,1), Matrix4x4 basis = Matrix4x4::identity(), Vector4 point = Vector4(0,0,0,0));
+    Scope(Vector4 point = Vector4(0,0,0,0), Vector4 scale = Vector4(1,1,1,1), Matrix4x4 basis = Matrix4x4::identity());
+    Scope(Vector4 point, Vector4 scale, Vector4 xbasis, Vector4 ybasis, Vector4 zbasis);
+
     Vector4 getPoint();
     Matrix4x4 getBasis();
     Vector4 getScale();
 
+    Scope translate(Vector4 v);
+    Scope setSize(Vector4 v);
+    Scope rotateX(REAL angle);
+    Scope rotateY(REAL angle);
+    Scope rotateZ(REAL angle);
 
 private:
     Vector4 m_point;
     Matrix4x4 m_basis;
     Vector4 m_scale;
+    Vector4 m_xbasis;
+    Vector4 m_ybasis;
+    Vector4 m_zbasis;
 };
 
 #endif // SCOPE_H
