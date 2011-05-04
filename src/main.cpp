2bcf475412ca0187f73751f8b49e3f26bf489624
hadrad1000@gmail.com <hadrad1000@gmail.com@cc3ac821-fe09-b41a-eedc-dc4eaaf1ceba>
2011-05-04 18:10:33 +0000

    mesh normals work

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,15 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    Referee ref("../data/grammar/first.gmr");
-    Feature* root = ref.blowWhistle();
+    //Referee ref("../data/grammar/first.gmr");
+    //Feature* root = ref.blowWhistle();
+    Feature* root = new Feature("window", "mesh", true, Scope(), NULL);
+    Mesh* m = new Mesh("../data/buddha.obj");
+
+    root->setMesh(m);
 
     w.setRoot(root);
+    root->draw();
 
     return a.exec();
 }
